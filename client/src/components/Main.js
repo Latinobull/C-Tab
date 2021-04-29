1ea9c8638b743e2f0aab580477de083af2622606
Donnahue George <Donnahuegjr@gmail.com>
2021-04-29 19:31:17 -0400

    adding main page to have someone choose their language for the page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,28 @@
+import { Button, Card, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 
 export default function Main() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      {/* <Grid container>
+        <Typography>Where are you</Typography>
+      </Grid> */}
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '90vh' }}
+      >
+        <Typography>
+          Welcome to Christian Tabernacle, Choose your Preferred Language
+        </Typography>
+        <Grid xs={12}>
+          <Button size="large">English</Button>
+          <Button size="large">Spanish</Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
