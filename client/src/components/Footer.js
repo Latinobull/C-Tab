6c5c35ee80332ad7e83312098626bd1363a1e4b9
Donnahue George <Donnahuegjr@gmail.com>
2022-06-16 12:13:12 -0400

    fixing footer to v5 of MUI

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,57 +1,53 @@
 import React from 'react';
-import { Typography, makeStyles, Grid } from '@material-ui/core';
-
-const useStyles = makeStyles(() => ({
-  footer: {
-    marginTop: 'auto',
-    backgroundColor: 'rgb(168,19,0)',
-    padding: '50px',
-  },
-  text: {
-    color: 'white',
-    justifyContent: 'space-evenly',
-    textAlign: 'center',
-  },
-}));
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+const MyFooter = styled('footer')`
+  margin-top: auto;
+  background-color: rgb(168, 19, 0);
+  padding: 50px;
+`;
+const FooterText = styled(Typography)`
+  color: white;
+`;
 export default function Footer() {
-  const classes = useStyles();
   return (
     <div>
-      <footer className={classes.footer}>
-        <Grid container className={classes.text}>
-          <Grid item>
-            <Typography variant="h6">
+      <MyFooter>
+        <Grid container justifyContent="space-evenly" textAlign="center">
+          <Grid item marginBottom="30px">
+            <FooterText variant="h6">
               CHRISTIAN TABERNACLE PENTECOSTAL CHURCH BROOKLYN
-            </Typography>
-            <Typography variant="h6">IGELSIA TABERNACULO CRISTIANO</Typography>
-            <Typography>
+            </FooterText>
+            <FooterText variant="h6">IGELSIA TABERNACULO CRISTIANO</FooterText>
+            <FooterText>
               616 JAMAICA AVENUE
               <br />
               BROOKLYN,NY 11208
               <br />
               (555) 555-5555
-            </Typography>
+            </FooterText>
           </Grid>
-          <Grid item lg={2}>
-            <Typography variant="h6">Services/Servicios</Typography>
-            <Typography>
+          <Grid item lg={2} marginBottom="30px">
+            <FooterText variant="h6">Services/Servicios</FooterText>
+            <FooterText>
               Sunday / Domingo - 7pm
               <br />
               Wednesday / Miercoles - 8pm
               <br />
               Friday / Viernes - 8pm
-            </Typography>
+            </FooterText>
           </Grid>
           <Grid item lg={2}>
-            <Typography variant="h6">Follow Us</Typography>
+            <FooterText variant="h6">Follow Us</FooterText>
 
             <a href="#" style={{ color: 'white' }}>
               Facebook
             </a>
-            <Typography>InsertEmail@gmail.com</Typography>
+            <FooterText>InsertEmail@gmail.com</FooterText>
           </Grid>
         </Grid>
-      </footer>
+      </MyFooter>
     </div>
   );
 }
