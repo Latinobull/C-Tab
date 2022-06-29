b74d3555e23bf5a3f4b1f2faf7e8fbbdc151da82
Donnahue George <Donnahuegjr@gmail.com>
2022-06-29 16:58:01 -0400

    mock background image for landing page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,30 +1,34 @@
 import React from 'react';
-import worshipImage from '../img/worship.jpg';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
-
-const MyBody = styled('div')`
-  background-image: url(${worshipImage});
-  background-size: cover;
-`;
+import backgroundImg from '../videos/welcomeTest.mp4';
 
 const MyContent = styled(Grid)`
-  background-color: rgb(255, 255, 255, 0.4);
   text-align: center;
   padding: 100px 50px 50px 50px;
+  color: white;
 `;
 
 const MyLink = styled(Link)`
-  color: black;
+  color: white;
   font-size: 20px;
+`;
+const MyVideo = styled('video')`
+  position: fixed;
+  z-index: -1;
+  width: 100%;
+  min-height: 100vh;
 `;
 
 export default function Welcome() {
   return (
-    <MyBody>
+    <div>
+      <MyVideo autoPlay loop muted id="video">
+        <source src={backgroundImg} type="video/mp4" />
+      </MyVideo>
       <Grid
         container
         direction="row"
@@ -61,6 +65,6 @@ export default function Welcome() {
           </Button>
         </MyContent>
       </Grid>
-    </MyBody>
+    </div>
   );
 }
