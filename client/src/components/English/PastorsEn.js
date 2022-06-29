db7869f916e0b23e55cf441c68c0be4d389c87b9
Donnahue George <Donnahuegjr@gmail.com>
2022-06-29 18:29:32 -0400

    adding new fonts to pastor page

--- client/src/components/English/PastorsEn.js
+++ client/src/components/English/PastorsEn.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import { Grid, styled, Typography } from '@mui/material';
+import headImg from '../../img/headShot.jpg';
+const MainGrid = styled(Grid)`
+  padding-bottom: 120px;
+`;
+const Headers = styled(Typography)`
+  text-align: center;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 600;
+  font-size: 50px;
+`;
 
+const Text = styled(Typography)`
+  text-align: center;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 300;
+  font-size: 30px;
+  max-width: 1400px;
+  padding-bottom: 30px;
+`;
+const LeaderImg = styled('img')`
+  padding-bottom: 25px;
+`;
 export default function PastorsEn() {
-  return <div>yellowHELLE</div>;
+  return (
+    <div>
+      <Headers>Meet the Pastors</Headers>
+      <MainGrid container>
+        <Grid container item md={12} justifyContent="center">
+          <LeaderImg
+            src={headImg}
+            sx={{
+              width: { xs: '300px', md: '450px' },
+              height: { xs: '400px', md: '500px' },
+            }}
+          ></LeaderImg>
+        </Grid>
+        <Grid container item md={12} justifyContent="center">
+          <Headers>Pastor Damaris and Huey Perez</Headers>
+        </Grid>
+        <Grid container item md={12} justifyContent="center">
+          <Text>
+            It all begins with an idea. Maybe you want to launch a business.
+            Maybe you want to turn a hobby into something more. Or maybe you
+            have a creative project to share with the world. Whatever it is, the
+            way you tell your story online can make all the difference.
+          </Text>
+        </Grid>
+        <Grid container item md={12} justifyContent="center">
+          <LeaderImg
+            src={headImg}
+            sx={{
+              width: { xs: '300px', md: '500px' },
+              height: { xs: '400px', md: '600px' },
+            }}
+          ></LeaderImg>
+        </Grid>
+      </MainGrid>
+    </div>
+  );
 }
