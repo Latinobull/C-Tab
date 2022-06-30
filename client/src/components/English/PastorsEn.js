c0465b36da1143cbd107024128563b95584147f0
Donnahue George <Donnahuegjr@gmail.com>
2022-06-29 21:35:39 -0400

    using map to render everything

--- client/src/components/English/PastorsEn.js
+++ client/src/components/English/PastorsEn.js
@@ -22,32 +22,44 @@ const Text = styled(Typography)`
 const LeaderImg = styled('img')`
   padding-bottom: 25px;
 `;
+
+const pastors = [
+  {
+    header: 'Pastor Damaris and Huey Perez',
+    content:
+      'It all begins with an idea. Maybe you want to launch a business.Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, theway you tell your story online can make all the difference.',
+    image: headImg,
+  },
+  {
+    header: 'Assistant Pastor Edward and Secretary Annette Guadalupe',
+    content:
+      'It all begins with an idea. Maybe you want to launch a business.Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, theway you tell your story online can make all the difference.',
+    image: headImg,
+  },
+];
 export default function PastorsEn() {
   return (
     <div>
-      <Headers>Meet the Pastors</Headers>
-      <MainGrid container>
-        <Grid container item md={12} justifyContent="center">
-          <LeaderImg
-            src={headImg}
-            sx={{
-              width: { xs: '300px', md: '450px' },
-              height: { xs: '400px', md: '500px' },
-            }}
-          ></LeaderImg>
-        </Grid>
-        <Grid container item md={12} justifyContent="center">
-          <Headers>Pastor Damaris and Huey Perez</Headers>
-        </Grid>
-        <Grid container item md={12} justifyContent="center">
-          <Text>
-            It all begins with an idea. Maybe you want to launch a business.
-            Maybe you want to turn a hobby into something more. Or maybe you
-            have a creative project to share with the world. Whatever it is, the
-            way you tell your story online can make all the difference.
-          </Text>
-        </Grid>
-      </MainGrid>
+      <Headers>Meet the Leaders</Headers>
+      {pastors.map(pastor => (
+        <MainGrid container>
+          <Grid container item md={12} justifyContent="center">
+            <LeaderImg
+              src={pastor.image}
+              sx={{
+                width: { xs: '300px', md: '400px' },
+                height: { xs: '400px', md: '500px' },
+              }}
+            ></LeaderImg>
+          </Grid>
+          <Grid container item md={12} justifyContent="center">
+            <Headers>{pastor.header}</Headers>
+          </Grid>
+          <Grid container item md={12} justifyContent="center">
+            <Text>{pastor.content}</Text>
+          </Grid>
+        </MainGrid>
+      ))}
     </div>
   );
 }
