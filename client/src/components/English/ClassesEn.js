1c345204cff46d78896c64502190d63032429a15
Donnahue George <Donnahuegjr@gmail.com>
2022-07-18 23:33:17 -0400

    fixing class component

--- client/src/components/English/ClassesEn.js
+++ client/src/components/English/ClassesEn.js
@@ -1,8 +1,11 @@
 import { Grid, styled, Typography } from '@mui/material';
+import laptopImg from '../../img/laptop.jpg';
 import React from 'react';
 const LiveGrid = styled(Grid)(({ theme }) => ({
   justifyContent: 'space-evenly',
   backgroundColor: theme.accent.main,
+  alignItems: 'center',
+  padding: '70px 0px',
 }));
 const Headers = styled(Typography)(({ theme }) => ({
   fontWeight: 600,
@@ -22,25 +25,55 @@ const BoldText = styled(Typography)(({ theme }) => ({
   fontSize: '23px',
   lineHeight: '60px',
 }));
+const Img = styled('img')({});
 export default function ClassesEn() {
   return (
     <div>
+      <Headers>Bible Studies (Virtual)</Headers>
+      <Text>
+        All our Bible studies are currently ran on zoom until further notice
+      </Text>
+      <BoldText>Men's Class - 12pm Sundays </BoldText>
       <LiveGrid container>
-        <Headers>Bible Studies (Virtual)</Headers>
+        <Grid item>
+          <Img src={laptopImg} sx={{ width: { xs: '300px', md: '600px' } }} />
+        </Grid>
+        <Grid item style={{ width: '500px' }}>
+          <BoldText>Led by:</BoldText>
+          <Text>Assistant Pastor Edward Guadalupe and Gaby Polidura</Text>
+          <BoldText>Zoom Link:</BoldText>
+          <Text>34744550148</Text>
+          <BoldText>Password:</BoldText>
+          <Text>234</Text>
+        </Grid>
       </LiveGrid>
-      <LiveGrid>
-        <Text>
-          All our Bible studies are currently ran on zoom until further notice
-        </Text>
-        <BoldText>
-          Men's Class - 12pm Sundays <br />
-          Led By:
-        </BoldText>
-        <Text>Pastor Edward Guadalupe, Gaby Polidura, Moyo Cintron</Text>
-        <BoldText>Zoom Link:</BoldText>
-        <Text>34744550148</Text>
-        <BoldText>Password:</BoldText>
-        <Text>234</Text>
+      <BoldText>Women's Class - 12pm Sundays </BoldText>
+      <LiveGrid container direction="row-reverse">
+        <Grid item>
+          <Img src={laptopImg} sx={{ width: { xs: '300px', md: '600px' } }} />
+        </Grid>
+        <Grid item style={{ width: '500px' }}>
+          <BoldText>Led by:</BoldText>
+          <Text>Pastor Damaris Torres and Annette Guadalupe</Text>
+          <BoldText>Zoom Link:</BoldText>
+          <Text>34744550148</Text>
+          <BoldText>Password:</BoldText>
+          <Text>234</Text>
+        </Grid>
+      </LiveGrid>
+      <BoldText>Youth Class - 12pm Sundays </BoldText>
+      <LiveGrid container>
+        <Grid item>
+          <Img src={laptopImg} sx={{ width: { xs: '300px', md: '600px' } }} />
+        </Grid>
+        <Grid item style={{ width: '500px' }}>
+          <BoldText>Led by:</BoldText>
+          <Text>Pastor Huey Perez and Michael Ruiz</Text>
+          <BoldText>Zoom Link:</BoldText>
+          <Text>34744550148</Text>
+          <BoldText>Password:</BoldText>
+          <Text>234</Text>
+        </Grid>
       </LiveGrid>
     </div>
   );
